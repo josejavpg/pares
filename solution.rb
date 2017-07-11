212dbcd6bc7ad769aa033af485cc31d78e5365a3
josejavpg <josejavpg@gmail.com>
2017-07-11 13:50:04 -0500

    Programa que identifica los números pares e impares del 1 al 50

---
+++ solution.rb
@@ -0,0 +1,17 @@
+require "sinatra"
+
+get '/' do 
+	@even = []
+	for i in 1..50
+		str = ""
+		if i.even?
+			str = "Par"
+		else
+			str = "Impar"
+		end
+		@even << "#{i} Soy #{str}"
+		#erb :index
+		#i
+	end
+	erb :index
+end
